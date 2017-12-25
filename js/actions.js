e80d29e4373c82c44e365c76312f3707d023bb6d
Ilia Sviryukov <sviryukov_iv@tkbbank.ru>
2017-12-25 09:29:32 +0300

    add sendmail

--- js/actions.js
+++ js/actions.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
 
+	$('form.order-form .button-order').on('click', function() {
+		$(this).closest('form').submit();
+	});
+
+	$('form.order-form').on('submit', function () {
+        var data = $(this).serialize(),
+            form = this;
+
+        $.ajax({
+            type: 'POST',
+            url: '/sendmail.php',
+            data: data,
+            response: 'text',
+            success: function(data) {
+                if(data == 'ok') {
+            		$('form.order-form').hide();
+            		$('.order .complete-message').show();
+                } else {
+                    var errors = data.split('|');
+                    if(errors[0] == '1') $('input[name="name"]', form).css('border-color', 'red');
+                    if(errors[1] == '1') $('input[name="phone"]', form).css('border-color', 'red');
+                }
+            }
+        });
+
+        return false;
+    });
+
+
 	$('.order-action').on('click', function(){
 				$('html, body').animate({
                     scrollTop: $(".order").offset().top
